06c8718071c3771ec366c97432dfa4cca596058b
rishikeshwagh17 <rishikesh.wagh1708@gmail.com>
2022-03-02 15:25:36 +0530

    adding additional files

--- My_solution/Sealed_Class/Program.cs
+++ My_solution/Sealed_Class/Program.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Sealed_Class;
 namespace Sealed_Class
 {
     //sealed class prevents inheritance
-
+    //you can create sealed class instance | --> used to prevent data to inherit to other class
     sealed class parent 
     {
         public void meth1()
@@ -28,8 +28,13 @@ namespace Sealed_Class
     {
         static void Main(string[] args)
         {
+            //creating sealed class instance
             parent parent = new parent();
             parent.meth1();
+            
+            
+            b obj1 = new b();
+            obj1.print();
             Console.ReadLine();
         }
     }
