c5a7c0606936f9f5f3698a0236cc2532d21cdcca
rishikeshwagh17 <rishikesh.wagh1708@gmail.com>
2022-03-02 15:23:21 +0530

    sealed method file added

---
+++ My_solution/Sealed_Class/SealedMethod.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sealed_Class
+{
+    public class a
+    {
+        public virtual void print() 
+        {
+            Console.WriteLine("a class method");
+        }
+    }
+
+    public class b :a
+    {
+        public sealed override void print() {
+            Console.WriteLine("this is b class sealed method");
+        }
+        
+        // if we mention the sealed on method then another class c which inherits b cannot
+        // override this sealed method
+        // try to create and see compile time error cannot use sealed method
+    }
+
+    
+}
