e92e3a02507ca54c786cd5bba51c6d5ee229e843
rishikeshwagh17 <rishikesh.wagh1708@gmail.com>
2022-03-02 19:31:21 +0530

    anonymous function conc file added

---
+++ OOPS/Anonymous_Function/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anonymous_Function
+{
+    //function with no names is called as a anonymous function & they are always sue with delegates
+    //anonymous function has only body use delegate keyword for declaring them
+    public delegate void delega(int num);
+    public class concepts 
+    {
+        public void mymethod(int a) 
+        {
+            a += 10;
+            Console.WriteLine(a);
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            delega obj = delegate (int a)
+            {
+                a = a + 25;
+                Console.WriteLine(a);
+            };
+            obj(10);
+            Console.ReadLine();        }
+    }
+}
