b2f74630d45ddbd849da2129b6b4efb2a4c0099d
rishikeshwagh17 <rishikesh.wagh1708@gmail.com>
2022-03-03 18:24:44 +0530

    anonymous func as a parameter to other func

---
+++ OOPS/Delegate_as_parameter/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate_as_parameter
+{
+    public delegate void del(int n);
+    public class myprogram
+    {
+        //myprogram.mymethod(delegate (int num)
+        //    {
+        //        Console.WriteLine(num);
+        //    }, "rishi");
+
+        //passing complete anonymous function as a mydel below  because delegate ref can hold anonymous function
+        //like  del obj = delegate(int n){
+        //          Console.WriteLine(n);
+        //      }
+    public void mymethod(del mydel, String name) {
+            Console.WriteLine("hello {0}",name);
+            mydel.Invoke(10);
+        }
+
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            myprogram myprogram = new myprogram();
+            myprogram.mymethod(delegate (int num)
+            {
+                Console.WriteLine(num);
+            }, "rishi");
+
+            Console.ReadLine();
+        }
+    }
+}
